47882ebe25f85fd653ca7aceae60c23461d31fed
linadells <kateryna.hilfanova.pz.2022@lpnu.ua>
2024-10-15 08:19:08 +0300

    Initialize dependencies in sl

--- lib/domain/usecases/send_result_usecase.dart
+++ lib/domain/usecases/send_result_usecase.dart
@@ -3,11 +3,11 @@ import 'package:test_project_webspark/core/data_state.dart';
 import 'package:test_project_webspark/core/usecase.dart';
 import 'package:test_project_webspark/domain/repository/find_route_repository.dart';
 
-class GetFieldInfoUseCase
+class SendResultUseCase
     implements UseCase<DataState<void>, Route> {
   final FindRouteRepository _findRouteRepository;
 
-  GetFieldInfoUseCase(this._findRouteRepository);
+  SendResultUseCase(this._findRouteRepository);
 
   @override
   Future<DataState<void>> call({Route? params}) {
